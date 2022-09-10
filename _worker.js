7fb43652c748ff5d0328e7b6040b4e5ec462146a
冰茶。 <44595436+Woohoy@users.noreply.github.com>
2022-09-10 14:04:44 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,31 +1,11 @@
-const whitelist = ["/bot5725571464:"];
-//示例const whitelist = ["/bot123456:"];
-const tg_host = "api.telegram.org";
-
-addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
-})
-
-function validate(path) {
-    for (var i = 0; i < whitelist.length; i++) {
-        if (path.startsWith(whitelist[i]))
-            return true;
+export default {
+  async fetch(request, env) {
+    let url = new URL(request.url);
+    if (url.pathname.startsWith('/')) {
+      url.hostname = 'tg.woohoy.workers.dev'
+      let new_request = new Request(url, request);
+      return fetch(new_request);
     }
-    return false;
-}
-
-async function handleRequest(request) {
-    var u = new URL(request.url);
-    u.host = tg_host;
-    if (!validate(u.pathname))
-        return new Response('Unauthorized', {
-            status: 403
-        });
-    var req = new Request(u, {
-        method: request.method,
-        headers: request.headers,
-        body: request.body
-    });
-    const result = await fetch(req);
-    return result;
-}
+    return env.ASSETS.fetch(request);
+  },
+};
